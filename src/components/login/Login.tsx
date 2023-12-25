7db36d8a6edeb68409497ee952d1c5c6aafd00e7
Musajiji <dzhal@bk.ru>
2023-12-25 21:57:40 +0300

    делаю форму для авторизации

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,3 +1,20 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormInput {
+  login: string;
+}
+
 export const Login = () => {
-  return <h1>войти</h1>;
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const dataForSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <>
+      <h1>войти</h1>
+      <form onSubmit={handleSubmit(dataForSubmit)}>
+        <label>login</label>
+        <input {...register("login")} />
+      </form>
+    </>
+  );
 };
