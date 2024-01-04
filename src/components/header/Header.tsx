9b9412f2eb9f08a28831812bbd80023606a09ac4
Musajiji <dzhal@bk.ru>
2024-01-04 21:05:03 +0300

    после регистрации кнопки регистрации и входа исчезают

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,42 @@
 import { Link } from "react-router-dom";
 
 export const Header = () => {
+  let user: string[][] = null;
+
+  if (localStorage.length > 0) {
+    let logData = localStorage.getItem("log");
+
+    if (logData) {
+      user = Object.entries(JSON.parse(logData));
+    }
+  }
+
   return (
     <header>
       <div>
         <Link to={"/"}>logo</Link>
       </div>
-      <ul>
-        <li>
-          <Link to={"/login"}>войти</Link>
-        </li>
-        <li>
-          <Link to={"/signup"}>зарегистрироваться</Link>
-        </li>
-      </ul>
+
+      {user ? (
+        <ul>
+          <li>{user[0][1]}</li>
+          <li>
+            <Link to={"/favorites"}>избранное</Link>
+          </li>
+          <li>
+            <Link to={"/history"}>история</Link>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to={"/login"}>войти</Link>
+          </li>
+          <li>
+            <Link to={"/signup"}>зарегистрироваться</Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 };
